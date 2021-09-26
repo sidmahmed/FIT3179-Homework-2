b1f52d9b8e7d56ff3c1cc4587d1a3b2e9dd69fe7
ahmedbonyeamin <ahmed.bonyeamin@gmail.com>
2021-09-26 21:30:58 +1000

    first commit

---
+++ vega_lite_vis.js
@@ -0,0 +1,5 @@
+var vg_1 = "state_map.vg.json";
+
+vegaEmbed("#viz", vg_1).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
